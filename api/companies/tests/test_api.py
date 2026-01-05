179d3b789c1e84317bdda5d6556acbba408904fe
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2026-01-05 16:51:40 +0000

    added setUp method to TestGetCompanies class

--- api/companies/tests/test_api.py
+++ api/companies/tests/test_api.py
@@ -5,12 +5,28 @@ from unittest import TestCase
 from django.test import Client
 from django.urls import reverse_lazy
 
+from companies.models import Company
+
 @pytest.mark.django_db
 class TestGetCompanies(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.companies_url = reverse_lazy("companies-list")
+
     def test_zero_companies_should_return_empty_list(self) -> None:
-        client = Client()
-        companies_url = reverse_lazy("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
+
+    def test_one_company_exists_should_succeed(self):
+        amazon = Company.objects.create(name="Amazon")
+        response = self.client.get(self.companies_url)
+        response_content = response.json()[0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_content.get("name"), "Amazon")
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+        amazon.delete()
