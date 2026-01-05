1f94851cc1f5666e5e5d27ff7e2b29addf75cf86
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2026-01-05 13:02:56 +0000

    started writing tests with unittest

---
+++ api/companies/tests/test_api.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestGetCompanies(TestCase):
+
+    def test_zero_companies_should_return_empty_list(self) -> None:
+        pass
--- manual_tests/test_api.py
+++ manual_tests/test_api.py
@@ -1,2 +1,33 @@
 import requests
 
+
+def pretty_print(data: list[dict]):
+    for item in data:
+        for key, value in item.items():
+            print(key, value)
+        print("-----")
+
+def get_companies_list():
+    response = requests.get(
+        url="http://127.0.0.1:8000/companies/"
+    )
+    return response.json()
+
+def add_company(name: str, status:str="Hiring", application_link: str="", notes: str=""):
+    response = requests.post(
+        url="http://127.0.0.1:8000/companies/",
+        json={
+            "name":name,
+            "status": status,
+            "application_link": application_link,
+            "notes": notes
+        }
+    )
+    return response
+
+def main():  
+    pretty_print(get_companies_list())
+
+
+if __name__ == "__main__":
+    main()
