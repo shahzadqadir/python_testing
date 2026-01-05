0b0fc60afff22431ae0375f555883cfe4f3533a4
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2026-01-05 08:20:18 +0000

    Added serializers and viewsets for api

--- api/companies/views.py
+++ api/companies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from api.companies.models import Company
+from .serializers import CompanySerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("last_update")
