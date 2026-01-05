addd22c674cb6e4813f865c48ae26c56be19ba9d
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2026-01-05 16:20:30 +0000

    Added pytest-db package, updated test file with decorator, updated requirements

--- api/companies/tests/test_api.py
+++ api/companies/tests/test_api.py
@@ -1,7 +1,16 @@
+import json
+import pytest
 from unittest import TestCase
 
+from django.test import Client
+from django.urls import reverse_lazy
 
+@pytest.mark.django_db
 class TestGetCompanies(TestCase):
 
     def test_zero_companies_should_return_empty_list(self) -> None:
-        pass
+        client = Client()
+        companies_url = reverse_lazy("companies-list")
+        response = client.get(companies_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), [])
