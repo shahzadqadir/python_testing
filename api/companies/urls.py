bb44b80d93ed643df31b6faf08d77267a5f29880
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2026-01-05 12:27:34 +0000

    added and tested urls, views for rest api

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -1,3 +1,7 @@
-from django.urls import path
+from rest_framework import routers
+
+from companies.views import CompanyViewSet
 
 
+companies_router = routers.DefaultRouter()
+companies_router.register("companies", viewset=CompanyViewSet, basename="companies")
--- api/companies/views.py
+++ api/companies/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
-from api.companies.models import Company
-from .serializers import CompanySerializer
+from companies.models import Company
+from companies.serializers import CompanySerializer
 
 
 class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by("last_update")
+    pagination_class = PageNumberPagination
