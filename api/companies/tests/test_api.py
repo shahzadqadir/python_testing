6c48dcc30b9797f4b3a4bcb05b3e04823dd223d2
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2026-01-06 05:13:45 +0000

    pytest api

--- api/companies/tests/test_api.py
+++ api/companies/tests/test_api.py
@@ -15,6 +15,9 @@ class BasicCompanyAPITestCase(TestCase):
         self.client = Client()
         self.companies_url = reverse_lazy("companies-list")
 
+    def tearDown(self):
+        pass
+
 
 class TestGetCompanies(BasicCompanyAPITestCase):
 
@@ -34,3 +37,44 @@ class TestGetCompanies(BasicCompanyAPITestCase):
         self.assertEqual(response_content.get("notes"), "")
 
         amazon.delete()
+
+
+class TestPostCompanies(BasicCompanyAPITestCase):
+
+    def test_create_company_without_arguments_should_fail(self) -> None:
+        response = self.client.post(
+            path=self.companies_url
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_existing_company_should_fail(self) -> None:
+        Company.objects.create(name="apple")
+        response = self.client.post(
+            path=self.companies_url, data={"name": "apple"}
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_company_with_only_name_all_other_fields_default(self):
+        response = self.client.post(
+            path=self.companies_url, data={"name": "blank"}
+        )
+        self.assertEqual(response.status_code, 201)
+        response_content = response.json()
+        self.assertEqual(response_content.get("name"), "blank")
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("notes"), "")
+
+    def test_create_company_with_wrong_status_should_fail(self):
+        response = self.client.post(
+            path=self.companies_url, data={"name": "newCompany", "status": "wrongStatus"}
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("is not a valid choice", str(response.content))
+
+    @pytest.mark.xfail
+    def test_known_bug_ok_to_fail(self):
+        self.assertEqual(1, 2)
+
+    @pytest.mark.skip
+    def test_known_bug_should_be_skipped(self):
+        pass
